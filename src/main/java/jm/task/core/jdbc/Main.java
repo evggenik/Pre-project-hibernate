7dbbf09a2bf25cf2ccab3ff41d0b305639996372
Evgeny Buchelnikov <gru554@yandex.ru>
2022-11-01 22:54:05 +1000

    The task 1.1.4 seems to be solved

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // реализуйте алгоритм здесь
+//        Util util = new Util();
+//        Util.getConnection();
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Иван", "Иванов", (byte) 22);
+        userService.saveUser("Петр", "Петров", (byte) 33);
+        userService.saveUser("Василий", "Сидоров", (byte) 11);
+        userService.saveUser("John", "Smith", (byte) 99);
+        System.out.println(userService.getAllUsers());
+//        userService.removeUserById(1);
+        userService.cleanUsersTable();
+//        System.out.println(userService.getAllUsers());
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testdatabase";
+    private static final String USERNAME = "test";
+    private static final String PASSWORD = "DkVsSA[<w92";
+    private static Connection connection;
+
+    public static Connection getConnect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
+
